2990cf600a92e69ecea568f93d87da62028ccd28
filipe.jesus <filipe.jesus@gx2-13.gx2.local>
2019-01-11 14:20:34 -0200

    Acerto nome do pct

---
+++ src/test/java/br/com/gx2/runners/RunnerTest.java
@@ -0,0 +1,30 @@
+package br.com.gx2.runners;
+
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		snippets = SnippetType.CAMELCASE,
+		plugin = {"pretty", 
+				"html:target/cucumber-html-report",
+				"json:target/cucumber.json", 
+				"junit:target/cucumber.xml",
+				"rerun:target/rerun.txt"},
+		features = "src/test/resources/features/MainFramelogin.feature",
+		glue = "steps"
+		)
+
+public class RunnerTest {
+
+    @BeforeClass
+    public static void setUpSuite() {
+        System.setProperty("jagacy.properties.dir",
+                "./src/test/resources");
+        System.setProperty("test.window", "true");
+    }
+}
